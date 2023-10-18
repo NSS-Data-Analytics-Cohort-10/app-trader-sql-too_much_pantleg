5539c7fce610c8ae2b2d82f3fe67b1f30221cfac
rushi <rushischokshi@gmail.com>
2023-10-17 21:00:01 -0500

    got queries with app traders purchase price column

--- scripts/apptrader_rushi.sql
+++ scripts/apptrader_rushi.sql
@@ -20,8 +20,16 @@ SELECT *,
 FROM play_store_apps AS p
 INNER JOIN app_store_apps AS a
 USING (name);
---This query gives us all apps that are on both app stores as well as a column that shows what App Trader's purchase price will be; the column is called real_price at the far right of the output.
+--This query gives us all apps that are on both app stores as well as a column that shows what App Trader's purchase price will be; the column is called real_price at the far right of the output. 553 rows
 
+SELECT *,
+	CASE WHEN CAST(p.price AS MONEY) > CAST(a.price AS MONEY) THEN CAST(p.price AS MONEY)*10000
+	WHEN CAST(a.price AS MONEY) > CAST(p.price AS MONEY) THEN CAST(a.price AS MONEY)*10000
+	ELSE CAST(10000 AS MONEY) END AS purchase_price
+FROM play_store_apps AS p
+FULL JOIN app_store_apps AS a
+USING (name);
+--This query does the same thing as the one above but it's a full join instead. 17709 rows
 
 SELECT *,
 	CASE WHEN CAST(price AS MONEY) > CAST(0 AS MONEY) THEN CAST(price AS MONEY)*10000
