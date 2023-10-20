853f220bd7b0f4d74b96b4e44a9b13bfa1bf8021
caleb.lucas <cplucas1133@gmail.com>
2023-10-19 20:39:05 -0500

    finished???

--- scripts/app_trader_sql.sql
+++ scripts/app_trader_sql.sql
@@ -129,10 +129,10 @@ SELECT DISTINCT name,
 	p.rating AS psa_rating,
 	CASE WHEN a.name = p.name THEN 'Yes' ELSE 'No'END AS in_both,
 	CASE WHEN CAST(p.price AS MONEY) > CAST(a.price AS MONEY) AND CAST(p.price AS MONEY) > CAST(1 AS MONEY) THEN CAST(p.price AS MONEY)*10000
-	WHEN CAST(a.price AS MONEY) > CAST(p.price AS MONEY) AND CAST(a.price AS MONEY) > CAST(1 AS MONEY) THEN CAST(a.price AS MONEY)*10000
-	WHEN CAST(p.price AS MONEY) = CAST(a.price AS MONEY) AND CAST(p.price AS MONEY) < CAST(1 AS MONEY) THEN CAST(10000 AS MONEY)
-	WHEN CAST(p.price AS MONEY) = CAST(a.price AS MONEY) AND CAST(p.price AS MONEY) > CAST(1 AS MONEY) THEN CAST(p.price AS MONEY)*10000
-	ELSE CAST(10000 AS MONEY) END AS purchase_price
+		WHEN CAST(a.price AS MONEY) > CAST(p.price AS MONEY) AND CAST(a.price AS MONEY) > CAST(1 AS MONEY) THEN CAST(a.price AS MONEY)*10000
+		WHEN CAST(p.price AS MONEY) = CAST(a.price AS MONEY) AND CAST(p.price AS MONEY) < CAST(1 AS MONEY) THEN CAST(10000 AS MONEY)
+		WHEN CAST(p.price AS MONEY) = CAST(a.price AS MONEY) AND CAST(p.price AS MONEY) > CAST(1 AS MONEY) THEN CAST(p.price AS MONEY)*10000
+		ELSE CAST(10000 AS MONEY) END AS purchase_price
 FROM (SELECT p.name, 
 	  (p.rating + a.rating)/2 AS avg_rating
 	  FROM play_store_apps AS p
