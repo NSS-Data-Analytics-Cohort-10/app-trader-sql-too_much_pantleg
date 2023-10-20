1283bab0221754e18c03b23077f4e900c65d4673
rushi <rushischokshi@gmail.com>
2023-10-19 20:42:59 -0500

    my team has our final query completed

--- scripts/apptrader_rushi.sql
+++ scripts/apptrader_rushi.sql
@@ -104,17 +104,41 @@ ORDER BY purchase_price;
 
 
 
-
-SELECT *,
-CAST(1000 AS MONEY) AS market_price,
-	CASE WHEN a.name = p.name THEN 'Yes' ELSE 'No'END AS in_both,
-	CASE WHEN CAST(p.price AS MONEY) > CAST(a.price AS MONEY) AND CAST(p.price AS MONEY) > CAST(1 AS MONEY) THEN CAST(p.price AS MONEY)*10000
-	WHEN CAST(a.price AS MONEY) > CAST(p.price AS MONEY) AND CAST(a.price AS MONEY) > CAST(1 AS MONEY) THEN CAST(a.price AS MONEY)*10000
-	WHEN CAST(p.price AS MONEY) = CAST(a.price AS MONEY) AND CAST(p.price AS MONEY) < CAST(1 AS MONEY) THEN CAST(10000 AS MONEY)
-	WHEN CAST(p.price AS MONEY) = CAST(a.price AS MONEY) AND CAST(p.price AS MONEY) > CAST(1 AS MONEY) THEN CAST(p.price AS MONEY)*10000
-	ELSE CAST(10000 AS MONEY) END AS purchase_price,
-	ROUND(ROUND(((p.rating+a.rating)/2)/5,1)*5,1) AS avg_rating, ((ROUND(ROUND(((p.rating+a.rating)/2)/5,1)*5,1)*2)+1) AS lifespan
-FROM play_store_apps AS p
+SELECT DISTINCT name,
+    ROUND(avg_rating / 0.5) * 0.5 AS averages,
+    ((ROUND(avg_rating / 0.5) * 0.5) * 24 + 12) AS lifespan_months,
+    CAST(((ROUND(avg_rating / 0.5) * 0.5) * 24 + 12) * 4000 AS MONEY) AS lifespan_income,
+    a.rating AS asa_rating,
+    p.rating AS psa_rating,
+    CASE WHEN a.name = p.name THEN 'Yes' ELSE 'No' END AS in_both,
+    purchase_price,
+    (CAST(((ROUND(avg_rating / 0.5) * 0.5) * 24 + 12) * 4000 AS MONEY) - purchase_price) AS profitability
+FROM (
+    SELECT p.name,
+        (p.rating + a.rating) / 2 AS avg_rating
+    FROM play_store_apps AS p
+    INNER JOIN app_store_apps AS a
+    USING (name)
+) AS avg_ratings
+INNER JOIN play_store_apps AS p
+USING (name)
 INNER JOIN app_store_apps AS a
 USING (name)
-ORDER BY name;
+LEFT JOIN (
+    SELECT p.name,
+        CASE
+            WHEN CAST(p.price AS MONEY) > CAST(a.price AS MONEY) AND CAST(p.price AS MONEY) > CAST(1 AS MONEY) THEN CAST(p.price AS MONEY) * 10000
+            WHEN CAST(a.price AS MONEY) > CAST(p.price AS MONEY) AND CAST(a.price AS MONEY) > CAST(1 AS MONEY) THEN CAST(a.price AS MONEY) * 10000
+            WHEN CAST(p.price AS MONEY) = CAST(a.price AS MONEY) AND CAST(p.price AS MONEY) < CAST(1 AS MONEY) THEN CAST(10000 AS MONEY)
+            WHEN CAST(p.price AS MONEY) = CAST(a.price AS MONEY) AND CAST(p.price AS MONEY) > CAST(1 AS MONEY) THEN CAST(p.price AS MONEY) * 10000
+            ELSE CAST(10000 AS MONEY)
+        END AS purchase_price
+    FROM play_store_apps AS p
+    INNER JOIN app_store_apps AS a
+    USING (name)
+) AS purchase_data
+USING (name)
+ORDER BY profitability DESC;
+
+
+
